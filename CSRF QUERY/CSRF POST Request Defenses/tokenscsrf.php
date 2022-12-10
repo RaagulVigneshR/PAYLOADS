59d29cea3dda6aa4dfafef4267b372126b787080
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-12-10 23:10:00 +0530

    Create tokenscsrf.php

---
+++ CSRF QUERY/CSRF POST Request Defenses/tokenscsrf.php
@@ -0,0 +1,7 @@
+<?php
+  function request_is_same_domain() {
+    if(!isset($_SERVER['HTTP_REFERER'])) { return false; }
+    $referer_host = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+    return ($referer_host === $_SERVER['HTTP_HOST']);
+  }
+?>
